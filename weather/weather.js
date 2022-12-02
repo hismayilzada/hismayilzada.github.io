b03af6e3b90b29032e8b87851b2a49bd65123efa
Humay <73535604+hismayilzada@users.noreply.github.com>
2022-12-03 00:59:35 +0400

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -70,27 +70,33 @@ function weatherDetails(info){
         const city = info.name;
         const country = info.sys.country;
         const {description, id} = info.weather[0];
-        const {temp, feels_like, humidity} = info.main;
+        const {temp, feels_like, humidity, pressure, wind} = info.main;
 
         if(id == 800){
-            wIcon.src = "icons/clear.svg";
+            wIcon.src = "clear.gif";
         }else if(id >= 200 && id <= 232){
-            wIcon.src = "icons/storm.svg";  
+            wIcon.src = "thunder.gif";  
         }else if(id >= 600 && id <= 622){
-            wIcon.src = "icons/snow.svg";
+            wIcon.src = "snow.gif";
         }else if(id >= 701 && id <= 781){
-            wIcon.src = "icons/haze.svg";
+            wIcon.src = "haze.gif";
         }else if(id >= 801 && id <= 804){
-            wIcon.src = "icons/cloud.svg";
+            wIcon.src = "cloud.gif";
         }else if((id >= 500 && id <= 531) || (id >= 300 && id <= 321)){
-            wIcon.src = "icons/rain.svg";
+            wIcon.src = "rain.gif";
         }
         
         weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
         weatherPart.querySelector(".weather").innerText = description;
         weatherPart.querySelector(".location span").innerText = `${city}, ${country}`;
+        var d = new Date();
+        document.getElementById("date").innerHTML = d.toLocaleDateString();
+        var t = new Date();
+        document.getElementById("time").innerHTML = t.toLocaleTimeString();
         weatherPart.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
         weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
+        weatherPart.querySelector(".pressure span").innerText = `${pressure}%`;
+        weatherPart.querySelector(".wind span").innerText = `${wind}%`;
         infoTxt.classList.remove("pending", "error");
         infoTxt.innerText = "";
         inputField.value = "";
